77198940574fd325006964baa459fece45049d17
ChaoHui Zheng <czheng4@vols.utk.edu>
2022-03-15 19:49:38 -0400

    GUI

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import subprocess
 from tkinter import *
 import functools
+import json
 
 # name conversion
 #Label   lbl lbl_name
@@ -12,6 +13,18 @@ import functools
 #Frame   frm frm_address
 
 
+
+
+def create_canvas(tab):
+    canvas = tk.Canvas(tab, width = 770,height = 550, scrollregion = (0, 0, 300, 1000))
+    canvas.pack(side=tk.LEFT)
+
+    scrollbar = tk.Scrollbar(tab, command=canvas.yview)
+    scrollbar.pack(side=tk.LEFT, fill='y', expand=True)
+
+    canvas.configure(yscrollcommand = scrollbar.set)
+
+    return canvas
 
 
 # Tab control class that allows you to add tabs and retrive tabs
@@ -88,21 +101,11 @@ class Terminal:
 
     def render(self):
 
-
-
-        canvas = tk.Canvas(self.tab, width = 770,height = 550, scrollregion = (0, 0, 300, 1000))
-        canvas.pack(side=tk.LEFT)
-
-        scrollbar = tk.Scrollbar(self.tab, command=canvas.yview)
-        scrollbar.pack(side=tk.LEFT, fill='y', expand=True)
-
-        canvas.configure(yscrollcommand = scrollbar.set)
-
+        canvas = create_canvas(self.tab)
+        
         # update scrollregion after starting 'mainloop'
         # when all widgets are in canvas
         canvas.bind('<Configure>', self.on_configure)
-        
-        # canvas.bind_all("<MouseWheel>", lambda event, canvas=canvas : on_mousewheel(event, canvas))
 
         # --- put frame in canvas ---
         self.frame = tk.Frame(canvas)
@@ -141,22 +144,156 @@ class UserCommands:
         self.window = window
 
 
-        self.texts = []       # all the texts
-        self.entries = []     # all the command entires to type in
-        self.all_commands = []
+        ## it's an array of array, the first index is the commnad id, the second index is t
+        self.command_widgets = []
+        self.vars = []
+
+        with open('data/commands.json') as f:
+            self.all_commands = json.load(f)
+       
+    def bound_to_mousewheel(self, event):
+        fp = functools.partial
+        self.canvas.bind_all("<Button-5>", fp(self.on_mousewheel, scroll=1))
+        self.canvas.bind_all("<Button-4>", fp(self.on_mousewheel, scroll=-1))
+       
+
+    def unbound_to_mousewheel(self, event):
+        self.canvas.unbind_all("<Button-4>")
+        self.canvas.unbind_all("<Button-5>")
+
+    def on_mousewheel(self, scroll):
+        self.canvas.yview_scroll(int(scroll), "units")
+
+
+    def on_configure(self, event):
+        # update scrollregion after starting 'mainloop'
+        # when all widgets are in canvas
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def getCommand(self, id):
         pass
 
 
+    def sel(self):
+        print(self.var.get())
+        # selection = "You selected the option " + str(var.get())
+        # label.config(text = selection)
+
+
+    def print_selection(self):
+        pass
+
     def render(self):
 
-        ttk.Label(self.tab,
-              text ="Lets dive into the\
-              world of computers").grid(column = 0,
-                                        row = 0, 
-                                        padx = 30,
-                                        pady = 30)
+
+        canvas = create_canvas(self.tab)
+        
+        # update scrollregion after starting 'mainloop'
+        # when all widgets are in canvas
+        canvas.bind('<Configure>', self.on_configure)
+        
+        # canvas.bind_all("<MouseWheel>", lambda event, canvas=canvas : on_mousewheel(event, canvas))
+
+        # --- put frame in canvas ---
+        self.frame = tk.Frame(canvas)
+
+        canvas.bind('<Enter>', self.bound_to_mousewheel)
+        canvas.bind('<Leave>', self.unbound_to_mousewheel)
+        canvas.create_window((0, 0), window=self.frame)
+        canvas.pack()
+        self.canvas = canvas
+
+
+        row_num = 0
+        for command in self.all_commands:
+
+            f =  tk.Frame(self.frame)
+            # print(command)
+            name = command["command_name"]
+            label = tk.Label(f, text = name, font=("Arial", 20)).pack(side = "left", anchor = 'w')
+            f.grid(row = row_num, column = 0, sticky="w")
+            row_num = row_num + 1
+
+
+            for name, widget in command["categories"].items():
+
+                f = tk.Frame(self.frame)
+                label = tk.Label(f, text = name + ": ").pack(padx = 10, side = "left", anchor = 'w')
+
+                f.grid(row = row_num, column = 0, sticky="w")
+                row_num = row_num + 1
+
+                wtype = widget["widget_type"]
+                if wtype == "radio_button":
+
+                    var = tk.IntVar(value=0)
+                    self.vars.append(var)
+                    # f.grid(row = 0, column = 0, sticky = 'w')
+                    for count, option in enumerate(widget["options"]):
+                        if wtype == "radio_button":
+                            tk.Radiobutton(f, text=option + "  ", variable=var, value=count).pack(side = "left", anchor = tk.W)
+                elif wtype == "checkbox":
+                    for count, option in enumerate(widget["options"]):
+                        var = tk.IntVar(value=0)
+                        self.vars.append(var)
+                        tk.Checkbutton(f, text=option + "  ",variable=var, onvalue=1, offvalue=0).pack(side = "left", anchor = tk.W)
+
+
+                elif wtype == "text":
+                    tk.Entry(f, width=55).pack(side = "left", anchor = tk.W)
+
+
+                # print(name, widget)
+                # for name, widget in category.items():
+
+                #     print(name, widget)
+
+
+            
+
+
+
+        # f =  tk.Frame(self.tab)
+        # self.var = tk.IntVar(value=1)
+        # var = self.var
+        # self.var.set(1)
+
+
+        # f.grid(row = 0, column = 0, sticky = 'w')
+        # R1 = tk.Radiobutton(f, text="Option 1", variable=var, value=1,
+        #                   command=self.sel)
+
+        # R1.pack( side = "left", anchor = tk.W )
+
+        # R2 = tk.Radiobutton(f, text="Option 2", variable=var, value=2,
+        #                   command=self.sel)
+
+        # R2.pack( side = "left", anchor = tk.W)
+
+        # R3 = tk.Radiobutton(f, text="Option 3", variable=var, value=3,
+        #                   command=self.sel)
+
+        # R3.pack( side = "left", anchor = tk.W)
+
+
+        # f = tk.Frame(self.tab)
+        # f.grid(row = 1, column = 0, sticky = 'w')
+        
+        # var1 = tk.IntVar()
+        # var2 = tk.IntVar()
+        
+        # c1 = tk.Checkbutton(f, text='Python',variable=var1, onvalue=1, offvalue=0, command=self.print_selection)
+        # c1.pack(side = "left", anchor = tk.W)
+        # c2 = tk.Checkbutton(f, text='C++',variable=var2, onvalue=1, offvalue=0, command=self.print_selection)
+        # c2.pack(side = "left")
+
+
+        # f = tk.Frame(self.tab)
+        # f.grid(row = 2, column = 0, sticky = 'w')
+        # ent = tk.Entry(master=f, width=55)
+        # ent.pack(side = "left")
+
+
 
 
 
